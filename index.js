d2c1a60060651c1fa1623bae657e74938aa55cb2
Luciano Caffaro <caffaroarielluciano@gmail.com>
2022-07-03 19:50:23 -0300

    add Grid Bootstrap

--- index.js
+++
@@ -1,6 +0,0 @@
-const navToggle = document.querySelector(".nav-toggle");
-const navMenu = document.querySelector(".nav-ul");
-
-navToggle.addEventListener("click", () => {
-	navMenu.classList.toggle("nav-ul_visible");
-});
